c16e623e2ee36c52d471f28b7a71c3fff108ffaf
Soliken <155744710+soliken1@users.noreply.github.com>
2024-11-15 20:17:59 +0800

    Vercel

--- api/routes.php
+++ api/routes.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::get('/users', [UserController::class, 'index']);
+Route::get('/', [UserController::class, 'index']);
+
 
