962321bc41eb3d5f4173a81374383a38032734cc
Soliken <155744710+soliken1@users.noreply.github.com>
2024-11-15 20:58:12 +0800

    Vercel

--- routes/api.php
+++ routes/api.php
@@ -2,4 +2,10 @@
 use App\Http\Controllers\UserController;
 
 Route::get('/users', [UserController::class, 'index']);
+Route::get('/test', function () {
+    return 'Test route is working!';
+});
+Route::get('/phpinfo', function () {
+    phpinfo();
+});
 
